eaa550c7360b98f341f8ae1b38fdfd6ed9f8f3a1
Trevor Bedson <trevobedson@gmail.com>
2026-05-26 14:30:54 -0700

    Initial Commit

---
+++ ZenATCTests/ZenATCTests.swift
@@ -0,0 +1,17 @@
+//
+//  ZenATCTests.swift
+//  ZenATCTests
+//
+//  Created by Trevor Bedson on 5/26/26.
+//
+
+import Testing
+@testable import ZenATC
+
+struct ZenATCTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
